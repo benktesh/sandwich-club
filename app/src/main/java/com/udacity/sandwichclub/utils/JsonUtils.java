4629a115db8d6bce15a133b40529ad8d7ed703b7
benktesh <benktesh@yahoo.com>
2018-04-24 20:33:41 -0500

    Updates utils
    Adds code to parse the json into Sandwich

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,68 @@
 package com.udacity.sandwichclub.utils;
 
+import android.support.annotation.NonNull;
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
+    //private String mainName; done
+    //private List<String> alsoKnownAs = null; done
+    //private String placeOfOrigin;
+    //private String description; done
+    //private String image; done
+    //private List<String> ingredients = null;
+    private static final String TAG = JsonUtils.class.getSimpleName();
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        try {
+
+
+            Sandwich sandwich = new Sandwich();
+
+            JSONObject object = new JSONObject(json);
+
+            //name is complex object
+            JSONObject nameObject = object.getJSONObject("name");
+            sandwich.setMainName(nameObject.getString("mainName"));
+            sandwich.setAlsoKnownAs(getStringsFromArray(nameObject.getString("alsoKnownAs")));
+
+            sandwich.setPlaceOfOrigin(object.getString("placeOfOrigin"));
+            sandwich.setDescription(object.getString("description"));
+            sandwich.setImage(object.getString("image"));
+            sandwich.setIngredients(getStringsFromArray(object.getString("ingredients")));
+
+            return sandwich;
+        }
+        catch(Exception ex)
+        {
+            Log.e(TAG, "Could not parse json " + json);
+            return null;
+        }
+    }
+
+
+
+    /**
+     * This method parses a json string to a list of string
+     * @param - String stringArray representation of json array
+     * @return List<String> list of string
+     * @throws JSONException
+     */
+    @NonNull
+    private static List<String> getStringsFromArray(String stringArray) throws JSONException {
+        List<String> list = new ArrayList<>();
+        JSONArray jsonarray = new JSONArray(stringArray);
+        for(int i = 0; i < jsonarray.length(); i++) {
+            list.add(jsonarray.getString(i));
+        }
+        return list;
     }
 }
