4234bb733cdca8f03cec60cb1b7181b1c1212898
Benktesh Sharma <benktesh@yahoo.com>
2018-04-26 16:48:40 -0500

    Incorporate review comments

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class JsonUtils {
 
     private static final String TAG = JsonUtils.class.getSimpleName();
+
     public static Sandwich parseSandwichJson(String json) {
         try {
 
@@ -24,38 +25,49 @@ public class JsonUtils {
             JSONObject object = new JSONObject(json);
 
             //name is complex object
-            JSONObject nameObject = object.getJSONObject("name");
-            sandwich.setMainName(nameObject.getString("mainName"));
-            sandwich.setAlsoKnownAs(getStringsFromArray(nameObject.getString("alsoKnownAs")));
+            JSONObject nameObject = object.optJSONObject("name");
+            sandwich.setMainName(nameObject.optString("mainName", "Not Found"));
+            sandwich.setAlsoKnownAs(getStringsFromArray(nameObject.optString("alsoKnownAs", "[\"\"]")));
 
-            sandwich.setPlaceOfOrigin(object.getString("placeOfOrigin"));
-            sandwich.setDescription(object.getString("description"));
+            sandwich.setPlaceOfOrigin(object.optString("placeOfOrigin", "Unknown"));
+            sandwich.setDescription(object.optString("description", "Not Found"));
             sandwich.setImage(object.getString("image"));
-            sandwich.setIngredients(getStringsFromArray(object.getString("ingredients")));
+            sandwich.setIngredients(getStringsFromArray(object.optString("ingredients", "[\"\"]")));
 
             return sandwich;
-        }
-        catch(Exception ex)
-        {
-            Log.e(TAG, "Could not parse json " + json);
+        } catch (Exception ex) {
+            Log.e(TAG + "parseSandwichJson", "Could not parse json " + json);
             return null;
         }
     }
 
 
-
     /**
      * This method parses a json string to a list of string
+     *
      * @param - String stringArray representation of json array
      * @return List<String> list of string
      * @throws JSONException
      */
     @NonNull
-    private static List<String> getStringsFromArray(String stringArray) throws JSONException {
-        List<String> list = new ArrayList<>();
-        JSONArray jsonarray = new JSONArray(stringArray);
-        for(int i = 0; i < jsonarray.length(); i++) {
-            list.add(jsonarray.getString(i));
+    private static List<String> getStringsFromArray(String stringArray) {
+
+        List<String> list = new ArrayList<String>();
+        try {
+
+
+            JSONArray jsonarray = null;
+
+            jsonarray = new JSONArray(stringArray);
+            for (int i = 0; i < jsonarray.length(); i++) {
+                list.add(jsonarray.optString(i, "Not Found"));
+            }
+            return list;
+        } catch (Exception ex)
+
+        {
+            Log.e(TAG + "getStringFromArray" + stringArray, ex.toString());
+
         }
         return list;
     }
